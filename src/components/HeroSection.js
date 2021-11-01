51e6693c7eddb9630d394b0055508289d962c0bd
Raghav <codingkapur@gmail.com>
2021-11-01 07:48:55 +0530

    update hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,9 +5,9 @@ const HeroSection = () => {
     <main className="hero-section__container">
       <div className="overlay">
         <div className="hero__text--container">
-          <p className="hero__text--title">Focus on your customers</p>
+          <p className="hero__text--title">Service your customers better</p>
           <p className="hero__text--subtitle">
-            While me make order management easy for you
+            with the best order management system in the world
           </p>
           <p className="hero__text--link">Learn more</p>
         </div>
